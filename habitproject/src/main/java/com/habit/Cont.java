2ac90b62ed47055fae90067d36914022ea020471
misung <misung@192.168.1.98>
2023-06-09 17:44:42 +0900

    first commit

---
+++ habitproject/src/main/java/com/habit/Cont.java
@@ -0,0 +1,12 @@
+package com.habit;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Cont {
+	
+	public Cont () {
+		System.out.println("-----Cont() 객체 생성됨");
+	}
+
+}
---
+++ habitproject/src/test/java/com/habit/HabitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
